e5f76c4e0d1a4c433dddb48a639dacc916e8d39f
Nani7008 <narimonjon05@gmail.com>
2024-04-02 14:08:18 +0500

    added functions file

---
+++ function.js
@@ -0,0 +1,138 @@
+
+
+
+
+// 1. Oy raqami kiritilsa Qaysi fasli ekanini aniqlovchi funksiya yozing
+
+
+// function fasl(oy) {
+
+//     switch (oy) {
+//         case 12: 
+//             oyFasl = "Dekabr QISH";
+//             break;
+//         case 1: 
+//             oyFasl = "Yanvar QISH";
+//             break;
+//         case 2: 
+//             oyFasl = "Fevral QISH";
+//             break;
+//         case 3: 
+//             oyFasl = "Mart BAHOR";
+//             break;
+//         case 4: 
+//             oyFasl = "Aprel BAHOR";
+//             break;
+//         case 5: 
+//             oyFasl = "May BAHOR";
+//             break;
+//         case 6: 
+//             oyFasl = "Iyun YOZ";
+//             break;
+//         case 7: 
+//             oyFasl = "Iyul YOZ";
+//             break;
+//         case 8: 
+//             oyFasl = "Avgust YOZ";
+//             break;
+//         case 9: 
+//             oyFasl = "Sentabr KUZ";
+//             break;
+//         case 10: 
+//             oyFasl = "Oktabr KUZ";
+//             break;
+//         case 11: 
+//             oyFasl = "Noyabr KUZ";
+//             break;
+//         default: 
+//             oyFasl = "Hatolik bunday oy raqami yo`q";
+//             break;
+//     }
+//     return oyFasl;
+// }
+
+// let oy = +prompt("Iltimos oy raqamini kiriting");
+// alert("Hozir " + fasl(oy));
+
+
+
+// 2.  Yoshingizni aniqlovchi funksiya yozing 
+
+
+// let birthdate=+prompt ("Iltimos tug`ilgan yillingizni kiriting")
+// let currentyear=+prompt ("Iltimos hozirgi yilni kiriting")
+// function birth (b, c){
+// let birthdate=c-b;
+// return birthdate;
+// }
+// alert ("Sizni yoshingiz " +birth(birthdate, currentyear) + " yoshda " )
+
+// 3. N gacha bo'lgan juft sonlar yig'indisini , N gacha bo'lgan toq sonlar yig'indisiga nisbatini aniqlovchi dastur tuzing. 
+
+
+
+// function juftSonlarYigindisi(n) {
+
+//     let s = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 == 0) {
+//             s += i;
+//         }
+      
+//     }
+//     return (s)
+
+// }
+
+
+// function toqSonlarYigindisi(n) {
+
+//     let s = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 !== 0) {
+//             s += i;
+//         }
+      
+//     }
+//    return (s)
+// }
+// let n = +prompt ("n sonini kriting")
+// console.log(juftSonlarYigindisi(n));
+// console.log (toqSonlarYigindisi(n));
+// console.log ((juftSonlarYigindisi(n))/(toqSonlarYigindisi(n)))
+
+// 4.  Telefon raqam kodiga qarab qaysi "Operator" ekanini aniqlovchi dastur yozing.
+
+// function operator(kod) {
+
+//     switch (kod) {
+//         case 99: 
+//             kodOperator= "Uzmobile";
+//             break;
+//         case 95: 
+//             kodOperator= "Uzmobile";
+//             break;
+//         case 97: 
+//             kodOperator= "UMS";
+//             break;
+//         case 93: 
+//             kodOperator= "Ucell";
+//             break;
+//        case 94: 
+//             kodOperator= "UMS";
+//             break;
+//        case 90: 
+//             kodOperator= "Beeline";
+//             break;
+//        case 91: 
+//             kodOperator= "Beeline";
+//             break;
+//     }
+//     return kodOperator;
+// }
+
+// let kod = +prompt("Iltimos kod raqamini kiriting");
+// alert(fasl(oy));
+
--- index.js
+++ index.js
@@ -138,3 +138,226 @@
 // console.log('ko`paytma: ', s); 
 
 
+// extra
+
+// IF 
+
+// if1 Butun son berilgan Agar berilgan son musbat bo`lsa 1 ga oshirlsin aks holda o`zgartrilmasin
+
+// let n = +prompt ('son=')
+
+// if (n>1) {
+//     alert(n+1);
+// }
+// else {
+//     alert (n)
+// } 
+
+
+// / if2 Butun son berilgan Agar berilgan son musbat bo`lsa 1 ga oshirlsin aks holda 2 ga kamaytrilsin
+
+// let n = +prompt ('son=')
+
+// if (n>1) {
+//     alert(n+1);
+// }
+// else {
+//     alert (n-2)
+// } 
+
+
+
+// / if3 Butun son berilgan Agar berilgan son musbat bo`lsa 1 ga oshirlsin agar manfiy bo`lsa 2 ga kamaytrilsin agar 0 ga teng bo`lsa 10ni ozlashtirsin
+
+// let n = +prompt ('son=')
+
+// if (n>1) {
+//     alert(n+1);
+
+// }
+// else if (n<0) {
+//     alert (n-2);
+// } 
+// else {
+//     alert (10)
+
+// }
+
+
+// / if4 Uchta butun son berilgan Shu sonlar orasidan nechta musbat son borligini aniqlovchi programma tuzing
+
+// let a = +prompt ('son=')
+// let b = +prompt ('son=')
+// let c = +prompt ('son=')
+
+// if (0<a && 0<b && 0<c) {
+
+//     alert (3 + "ta")
+// }
+
+// else if (0>a && 0<b && 0<c) {
+//     alert (2 + "ta")
+
+// }
+
+// else if (0<a && 0>b && 0<c) {
+//     alert (2 + "ta")
+
+// }
+
+// else if (0<a && 0<b && 0>c) {
+//     alert (2 + "ta")
+
+// }
+
+// else if (0>a && 0>b && 0<c) {
+//     alert (1 + "ta")
+
+// }
+
+// else if (0>a && 0<b && 0>c) {
+//     alert (1 + "ta")
+
+// }
+
+// else if (0>a && 0<b && 0>c) {
+//     alert (1 + "ta")
+
+// }
+
+// else {
+//     alert (0)
+// }
+
+
+
+// if6 Ikkita butun son berilgan Shu sonlarning kattasini aniqlovchi programma tuzilsin
+
+// let a = +prompt ('a=')
+// let b = +prompt ('b=')
+
+// if (a>b) {
+//  alert (a + " eng katta " )
+
+
+// }
+
+// else {
+//    alert (b + " eng katta " )
+// }
+
+
+// if7 Ikkita butun son berilgan Shu sonlarning kichigini tartib raqamini  aniqlovchi programma tuzilsin
+
+
+// let a = +prompt ('a=')
+// let b = +prompt ('b=')
+
+// if (a<b) {
+//  alert (1)
+
+// }
+
+// else {
+//    alert (2 )
+// }
+
+
+
+
+// if 8
+// let a = +prompt ('a=')
+// let b = +prompt ('b=')
+
+// if (a>b) {
+//  alert (a, b)
+
+// }
+
+// else {
+//    alert (b, a )
+// // }
+
+// let watterPrice=2100;
+// for(let i=10; i<=20; i++) {
+//     console.log (`${i/10}L watter = ${i/10*watterPrice}soms`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 11. N  (N>0) butun son berilgan. Quyidagi yig‗indini hisoblovchi algoritm tuzilsin: S=N2+ (N+1)2+(N+2)2+ . . . + (2N) 
+
+// function Sum () {
+
+// let n = +prompt('n sonini kiriting=');
+// let s = 0;
+// for (let i = n; i <= n * 2; i++) {
+//     s += i * i;
+ 
+// }
+// console.log('a va b b gacha bo`lgan butun sonlar kvadratining yig`indisi: ', s); 
+
+// }
+
+
+// Sum () 
+
+// // 12
+
+
+
+// let n = +prompt('n sonini kiritng');
+
+// function k(num) {
+
+
+// let s = 1;
+
+
+// for (let i = 1.1; i <=num; i+=0.1) {
+//     s *= i; 
+// }
+
+// console.log('ko`paytma: ', s); 
+
+
+// }
+
+// k (n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
