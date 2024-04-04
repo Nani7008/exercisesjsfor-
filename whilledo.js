a3ec78a87024fef8392d946e98c188d64c9d1408
Nani7008 <narimonjon05@gmail.com>
2024-04-04 12:32:10 +0500

    added while/do examples

---
+++ whilledo.js
@@ -0,0 +1,135 @@
+// // 11. 1 dan 10 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// //  12. 10 dan 1 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let j = 10;
+// while (j >= 1) {
+//   console.log(j);
+//   j--;
+// }
+// // 13. 1 dan 10 gacha bo'lgan juft sonlarni konsolga chiqaring
+
+
+// let k = 1;
+// while (k <= 10) {
+//   if (k % 2 === 0) {
+//     console.log(k);
+//   }
+//   k++;
+// }
+// // 14. 1 dan 100 gacha bo'lgan sonlarning yig'indisini hisoblang
+
+// let sum = 0;
+// let n = 1;
+// while (n <= 100) {
+//   sum += n;
+//   n++;
+// }
+// console.log("1 dan 100 gacha bo'lgan sonlar yig'indisi: " + sum);
+
+// // 15. 1 dan 10 gacha bo'lgan sonlar darajasini hisoblang
+
+
+// let m = 1;
+// while (m <= 10) {
+//   console.log(m + "-ning darajasi: " + (m * m));
+//   m++;
+// }
+// 16. 20 dan 30 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let a = 20;
+// while (a <= 30) {
+//   console.log(a);
+//   a++;
+// }
+// 17. 50 dan 40 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let b = 50;
+// while (b >= 40) {
+//   console.log(b);
+//   b--;
+// }
+// 18. 1 dan 20 gacha bo'lgan toq sonlarni konsolga chiqaring
+
+
+// let c = 1;
+// while (c <= 20) {
+//   if (c % 2 !== 0) {
+//     console.log(c);
+//   }
+//   c++;
+// }
+// 19. 1 dan 50 gacha bo'lgan sonlarning juftlarining yig'indisini hisoblang
+
+
+// let a = 1;
+// let sumJuft = 0;
+// while (a <= 50) {
+// if (a % 2 === 0) {
+// sumJuft += a;
+//   }
+//   a++;
+// }
+// console.log("1 dan 50 gacha bo'lgan juft sonlar yig'indisi: " + sumJuft);
+// 20. 1 dan 10 gacha bo'lgan sonlarning kvadratlarini konsolga chiqaring
+
+
+// let a = 1;
+// while (a <= 10) {
+//   console.log(a + "-ning kvadrati: " + (a * a));
+//   a++;
+// }
+// WHILE DO
+// 21. 1 dan 41 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 41);
+// 22. 11 dan 1 gacha bo'lgan sonlarni konsolga chiqaring
+
+
+// let a = 11;
+// do {
+//   console.log(a);
+//   j--;
+// } while (a >= 1);
+// 23. 1 dan 70 gacha bo'lgan juft sonlarni konsolga chiqaring
+
+
+// let a = 1;
+// do {
+//   if (a % 2 === 0) {
+//     console.log(a);
+//   }
+//   k++;
+// } while (a <= 70);
+// 24. 1 dan 400 gacha bo'lgan sonlarning yig'indisini hisoblang
+
+
+// let sum = 0;
+// let n = 1;
+// do {
+//   sum += n;
+//   n++;
+// } while (n <= 400);
+// console.log("1 dan 100 gacha bo'lgan sonlar yig'indisi: " + sum);
+// 25. 1 dan 30 gacha bo'lgan sonlar darajasini hisoblang
+
+
+// let n = 1;
+// do {
+//   console.log(n + "-ning darajasi: " + (n * n));
+//   m++;
+// } while (n <= 30);
